5612560c2930f81cb368062565ade54205e8aae1
Ben <mira.ben@openmailbox.org>
2014-10-16 15:30:23 +0200

    Fichiers Pythons nettoyés (tab -> space) et gitignore modifié pour ignorer les .pyc

--- Character.py
+++ Character.py
@@ -1,48 +1,48 @@
 import math
 
 class Character:
-	"""A class for Characters."""
-	def __init__(self, name, race, constitution, agility, esprit, sensitivity, attractness, rage, mystery, blood_thirst, greed_lvl, relations):
-		self.name = name										# String
-		self.race = race										# String
-		self.constitution = constitution						# Integer [0,10]
-		self.agility = agility									# Integer [0,10]
-		self.esprit = esprit									# Integer [0,10]
-		self.sensitivity = sensitivity							# Integer [0,10]
-		self.attractness = attractness							# Integer [0,10]
-		self.rage = rage										# Integer [0,10]
-		self.mystery = mystery									# Integer [0,10]
-		self.blood_thirst = blood_thirst						# Integer [0,10]
-		self.greed_lvl = greed_lvl								# Integer [0,10]
-		self.relations = relations								# Dictionnaire
-		self.RACE_RELATIONS = self.getRaceRelations()					# Dictionnaire (A transformer en classe)
+    """A class for Characters."""
+    def __init__(self, name, race, constitution, agility, esprit, sensitivity, attractness, rage, mystery, blood_thirst, greed_lvl, relations):
+        self.name = name                                # String
+        self.race = race                                # String
+        self.constitution = constitution                # Integer [0,10]
+        self.agility = agility                          # Integer [0,10]
+        self.esprit = esprit                            # Integer [0,10]
+        self.sensitivity = sensitivity                  # Integer [0,10]
+        self.attractness = attractness                  # Integer [0,10]
+        self.rage = rage                                # Integer [0,10]
+        self.mystery = mystery                          # Integer [0,10]
+        self.blood_thirst = blood_thirst                # Integer [0,10]
+        self.greed_lvl = greed_lvl                      # Integer [0,10]
+        self.relations = relations                      # Dictionnaire
+        self.RACE_RELATIONS = self.getRaceRelations()   # Dictionnaire (A transformer en classe)
 
-	def __repr__(self):
-		return("Je m'appelle {}".format(self.name))
+    def __repr__(self):
+        return("Je m'appelle {}".format(self.name))
 
-	def getRaceRelations(self):
-		if self.race == 'humain':
-			return({'humain': -5})
+    def getRaceRelations(self):
+        if self.race == 'humain':
+            return({'humain': -5})
 
-	def getInfluence(self):
-		at_blth = (self.attractness + self.blood_thirst)/4 
-		if at_blth == 0:
-			return 0
-		else:
-			bonus_myst = self.mystery*(4/10)
-			return(int((at_blth + (bonus_myst)/2)))
+    def getInfluence(self):
+        at_blth = (self.attractness + self.blood_thirst)/4 
+        if at_blth == 0:
+            return 0
+        else:
+            bonus_myst = self.mystery*(4/10)
+            return(int((at_blth + (bonus_myst)/2)))
 
-	def getObedience(self, character):
-		influence = character.getInfluence()
-		if self.RACE_RELATIONS[character.race] <= -5:
-			return(int(self.RACE_RELATIONS[character.race] + influence))
-		elif self.RACE_RELATIONS[character.race] > -5 and self.RACE_RELATIONS[character.race] <= 0:
-			return(int(self.RACE_RELATIONS[character.race] + influence/1.3))
-		elif self.RACE_RELATIONS[character.race] > 0 and self.RACE_RELATIONS[character.race] <= 5:
-			return(int(self.RACE_RELATIONS[character.race] + influence/2))
-		else:
-			return(int(self.RACE_RELATIONS[character.race] + influence/3))
+    def getObedience(self, character):
+        influence = character.getInfluence()
+        if self.RACE_RELATIONS[character.race] <= -5:
+            return(int(self.RACE_RELATIONS[character.race] + influence))
+        elif self.RACE_RELATIONS[character.race] > -5 and self.RACE_RELATIONS[character.race] <= 0:
+            return(int(self.RACE_RELATIONS[character.race] + influence/1.3))
+        elif self.RACE_RELATIONS[character.race] > 0 and self.RACE_RELATIONS[character.race] <= 5:
+            return(int(self.RACE_RELATIONS[character.race] + influence/2))
+        else:
+            return(int(self.RACE_RELATIONS[character.race] + influence/3))
 
-	def getLoyalty(self, character):
-		return(self.getObedience(character) + (self.greed_lvl - 5))
+    def getLoyalty(self, character):
+        return(self.getObedience(character) + (self.greed_lvl - 5))
 
--- main.py
+++ main.py
@@ -1,33 +1,33 @@
 from Character import *
 
 gloglo = Character(
-				'Glorwynn',	# Nom
-				'humain',	# Race
-				0,			# Carrure
-				0,			# Adresse
-				0,			# Esprit
-				0,			# Sens
-				10,			# Attraits
-				0,			# Ravages
-				10,			# Mystere
-				10,			# Soif de sang
-				5,			# Avidite
-				{'Toto': 3}	# Relations
-				)
+                'Glorwynn',     # Nom
+                'humain',       # Race
+                0,              # Carrure
+                0,              # Adresse
+                0,              # Esprit
+                0,              # Sens
+                10,             # Attraits
+                0,              # Ravages
+                10,             # Mystere
+                10,             # Soif de sang
+                5,              # Avidite
+                {'Toto': 3}     # Relations
+                )
 toto = Character(
-				'Toto',			# Nom
-				'humain',		# Race
-				5,				# Carrure
-				5,				# Adresse
-				5,				# Esprit
-				5,				# Sens
-				5,				# Attraits
-				5,				# Ravages
-				5,				# Mystere
-				5,				# Soif de sang
-				0,				# Avidite
-				{'Glowynn': 3}	# Relations
-				)
+                'Toto',
+                'humain',
+                5,
+                5,
+                5,
+                5,
+                5,
+                5,
+                5,
+                5,
+                0,
+                {'Glowynn': 3}
+                )
 
 print(gloglo.getInfluence())
 print(toto.getObedience(gloglo))
